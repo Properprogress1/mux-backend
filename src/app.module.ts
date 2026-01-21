895781611879e3daec8963b906655fbe1ad5e035
delfinacorr <delfinacorradini073@gmail.com>
2026-01-21 14:12:41 -0300

    feat(scaffold): initialize core modules for invisible wallet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WalletsModule } from './wallets/wallets.module';
+import { PaymentsModule } from './payments/payments.module';
+import { LimitsModule } from './limits/limits.module';
+import { RecoveryModule } from './recovery/recovery.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, WalletsModule, PaymentsModule, LimitsModule, RecoveryModule],
   controllers: [AppController],
   providers: [AppService],
 })
